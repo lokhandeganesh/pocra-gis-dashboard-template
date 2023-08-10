1d3e2c032962add1812a161f702926acb0d8bf7c
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-08-10 18:09:31 +0530

    change in layout

--- js/dbt-service-js/pocra-dbt-nrm-map.js
+++ js/dbt-service-js/pocra-dbt-nrm-map.js
@@ -2,20 +2,6 @@ window.onload = init;
 
 function init() {
 
-  const map = new ol.Map({
-    view: new ol.View({
-      center: [77.5, 18.95],
-      zoom: 7.2,
-      projection: 'EPSG:4326'
-    }),
-    target: 'pocra-dbt-nrm-map'
-  })
-
-
-  // map.on('click', function (e) {
-  //   console.log(e.coordinate);
-  // })
-
   // Adding control to layer switcher
   const baseLayerGroup = new ol.layer.Group({
     title: 'Base Layers',
@@ -49,41 +35,12 @@ function init() {
         type: 'base',
 
       }),
-      new ol.layer.Tile({        
-        source: new ol.source.TileWMS({
-          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard_V2/wms',
-          crossOrigin: 'Anonymous',
-          serverType: 'geoserver',          
-          params: {
-            'LAYERS': 'PoCRA_Dashboard_V2:nrm_point_data_existing_structures',
-            'TILED': true,
-          }
-        }),
-        visible: false,
-        baseLayer: false,
-        title: "NRM Existing Structures",
-      }),
-      new ol.layer.Tile({        
-        source: new ol.source.TileWMS({
-          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard_V2/wms',
-          crossOrigin: 'Anonymous',
-          serverType: 'geoserver',
-          
-          params: {
-            'LAYERS': 'PoCRA_Dashboard_V2:nrm_point_data_pocra_structures',
-            'TILED': true,
-          }
-        }),
-        visible: false,
-        baseLayer: false,
-        title: "NRM PoCRA Structures",
-      }),
-      new ol.layer.Tile({       
+      new ol.layer.Tile({
         source: new ol.source.TileWMS({
           url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard/wms',
           crossOrigin: 'Anonymous',
           serverType: 'geoserver',
-          
+
           params: {
             'LAYERS': 'PoCRA:MahaDist',
             'TILED': true,
@@ -96,9 +53,135 @@ function init() {
     ]
   })
 
+  const adminLayers = new ol.layer.Group({
+    title: 'Administrative Layers',
+    openInLayerSwitcher: true,
+    layers: [
+      new ol.layer.Tile({
+        // extent: extentforLayer,    
+        // type: type,
+        source: new ol.source.TileWMS({
+          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard/wms',
+          crossOrigin: 'Anonymous',
+          serverType: 'geoserver',
+          params: {
+            'LAYERS': 'PoCRA:Settlement',
+            'TILED': true
+          }
+        }),
+        visible: false,
+        baseLayer: false,
+        title: "Settlements",
+      }),
+      new ol.layer.Tile({
+        // extent: extentforLayer,    
+        // type: type,
+        source: new ol.source.TileWMS({
+          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard/wms',
+          crossOrigin: 'Anonymous',
+          serverType: 'geoserver',
+          params: {
+            'LAYERS': 'PoCRA:Road',
+            'TILED': true
+          }
+        }),
+        visible: false,
+        baseLayer: false,
+        title: "Roads",
+      }),
+      new ol.layer.Tile({
+        // extent: extentforLayer,    
+        // type: type,
+        source: new ol.source.TileWMS({
+          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard/wms',
+          crossOrigin: 'Anonymous',
+          serverType: 'geoserver',
+          params: {
+            'LAYERS': 'PoCRA:River',
+            'TILED': true
+          }
+        }),
+        visible: false,
+        baseLayer: false,
+        title: "Rivers",
+      }),
+      new ol.layer.Tile({
+        // extent: extentforLayer,    
+        // type: type,
+        source: new ol.source.TileWMS({
+          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard/wms',
+          crossOrigin: 'Anonymous',
+          serverType: 'geoserver',
+          params: {
+            'LAYERS': 'PoCRA:Waterbody',
+            'TILED': true
+          }
+        }),
+        visible: false,
+        baseLayer: false,
+        title: "Waterbody",
+      }),
+
+
+    ]
+  });
+
+  // A group layer for Administrative layers
+  const activityLayers = new ol.layer.Group({
+    title: 'Activity Layers',
+    openInLayerSwitcher: true,
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard_V2/wms',
+          crossOrigin: 'Anonymous',
+          serverType: 'geoserver',
+          params: {
+            'LAYERS': 'PoCRA_Dashboard_V2:nrm_point_data_existing_structures',
+            'TILED': true,
+          }
+        }),
+        visible: false,
+        baseLayer: false,
+        title: "NRM Existing Structures",
+      }),
+      new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+          url: 'http://gis.mahapocra.gov.in/geoserver/PoCRA_Dashboard_V2/wms',
+          crossOrigin: 'Anonymous',
+          serverType: 'geoserver',
+
+          params: {
+            'LAYERS': 'PoCRA_Dashboard_V2:nrm_point_data_pocra_structures',
+            'TILED': true,
+          }
+        }),
+        visible: false,
+        baseLayer: false,
+        title: "NRM PoCRA Structures",
+      }),
+
+    ]
+  });
+
+  // The Map
+  const map = new ol.Map({
+    view: new ol.View({
+      center: [77.5, 18.95],
+      zoom: 7.2,
+      projection: 'EPSG:4326'
+    }),
+    target: 'pocra-dbt-nrm-map',
+    layers: [baseLayerGroup, adminLayers, activityLayers]
+  })
+
+
+  // map.on('click', function (e) {
+  //   console.log(e.coordinate);
+  // })
 
   // Adding Base Layer to Map
-  map.addLayer(baseLayerGroup);
+  // map.addLayer(baseLayerGroup);
 
   // Layer Switcher Extention
   const layerSwitcher = new ol.control.LayerSwitcher({
